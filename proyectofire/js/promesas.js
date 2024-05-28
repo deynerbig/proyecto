1c81fcd2739dee736034ed6d5128317c1611a128
deynerbig <169170084+deynerbig@users.noreply.github.com>
2024-05-28 11:33:42 -0400

    hola

--- proyectofire/js/promesas.js
+++ proyectofire/js/promesas.js
@@ -1,3 +1,20 @@
+//aca falta el import
 export const registrarPersona=(persona)=>{
     console.log(persona);
+}
+//nuevo
+export const odtenerPersona=async(persona)=>{
+    //recupera la referencia(ruta)
+    const ref=collection(db,"personas");
+    //recupera una captura de db
+    const qSnap=await getDocs(ref);
+    let listado=[]
+    //recorrer la base de datos y que lo devuelva en un listado
+    qSnap.forEach((doc) => {
+        //console.log(doc.id);
+        console.log(doc.data());
+        listado.push({...doc.data(),id:doc.id})
+    });
+    console.log(listado);
+    return listado;
 }
--- proyectofire/js/script.js
+++ proyectofire/js/script.js
@@ -1,14 +1,62 @@
-import{registrarPersona} from "./promesas.js";
+//obtener persona es lo nuevo 
+import{registrarPersona,obtenerPersona} from "./promesas.js";
 window.addEventListener("load",()=>{
-    document.getElementById("btnRegistrar").addEventListener("click",registrar)});
+    document.getElementById("btnRegistrar").addEventListener("click",registrar)
+    //nuevo
+   cargarDatos(); 
+});
 const registrar=()=>{
     //recupero el elemento
     let eNombre=document.getElementById("nombre");
+    let eapellido=document.getElementById("apellido");
+    let erut=document.getElementById("rut");
+    let ecorreo=document.getElementById("correo");
+    let eedad=document.getElementById("edad");
+    let efecha=document.getElementById("fecha");
     //recupero valor del elemento
     let vNombre=eNombre.value;
+    let vapellido=eapellido.value;
+    let vrut=erut.value;
+    let vcorreo=ecorreo.value;
+    let vedad=eedad.value;
+    let vfecha=efecha.value;
     //creo un objeto con los datos recuperados
-    let objeto={nombre:vNombre}
+    let objeto={
+        nombre:vNombre,
+        apellido:vapellido,
+        rut:vrut,
+        correo:vcorreo,
+        edad:vedad,
+        fecha:vfecha
+    }
     //envio a una funcion que registre 
     console.log(objeto)
-    registrarPersona(objeto)
+    registrarPersona(objeto).then(()=>{
+        alert("se registro con exito")
+        cargarDatos()
+    })
+}
+//nuevo
+const cargarDatos =()=>{
+//traer de las promesas todo lo registrado
+    obtenerPersona().then((personas)=>{
+        console.log("hola")
+        console.log(personas)
+        //cargarlo en la tabla de html
+        let estructura=""
+        personas.forEach((p)=>{
+            estructura+="<tr>"
+            estructura+="<td>"+p.nombre+"</td>"
+            estructura+="<td>"+p.apellido+"</td>"
+            estructura+="<td>"+p.rut+"</td>"
+            estructura+="<td>"+p.correo+"</td>"
+            estructura+="<td>"+p.edad+"</td>"
+            estructura+="<td>"+p.fecha+"</td>"
+            estructura+="<td><button '"+p.id+"'>actualizar</button></td>"
+            estructura+="<td><button >eliminar</button></td>"
+            estructura+="</tr>"
+        });
+        document.getElementById("cuerpoTabla").innerHTML=estructura;
+    })
+//cargarlo en la tabla del html
 }
